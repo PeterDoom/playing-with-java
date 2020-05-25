ace4e6ab91cd6236195ec95041487583441a6aae
PeterDoom <nanev0205@gmail.com>
2020-05-26 01:13:03 +0300

    Example for encapsulation finished

---
+++ HelloWorld/src/oop/encapsulation/EncapsulationMain.java
@@ -0,0 +1,14 @@
+package oop.encapsulation;
+
+public class EncapsulationMain {
+    public static void main(String[] args) {
+        PlayerEncapsulation player = new PlayerEncapsulation("Tim", 100, "Sword");
+
+
+        int damage = 10;
+        player.loseHealth(damage);
+        System.out.println("Ramaining health = " + player.getHealth());
+    }
+
+
+}
