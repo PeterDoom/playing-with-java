b0b299fc8a6bf49f588524ff9fcede3fdba00473
PeterDoom <nanev0205@gmail.com>
2020-05-25 23:57:21 +0300

    Example for composition finished

---
+++ HelloWorld/src/oop/composition/CompositionMain.java
@@ -0,0 +1,17 @@
+package oop.composition;
+
+public class CompositionMain {
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20, 20 ,5);
+        Case pcCase = new Case("220b", "dell", "230", dimensions);
+
+        Resolution nativeResolution = new Resolution(1920, 1080);
+        Monitor monitor =new Monitor("27 inch", "acer", 25, nativeResolution);
+        Motherboard theMotherBoard = new Motherboard("SOMEMODEL-2", "Asus", 4, 6, "v2");
+
+        PC pc = new PC(pcCase, monitor, theMotherBoard);
+        pc.getPcCase().pressPowerButton();
+        pc.getMonitor().drawPixelAt(1500, 1200 , "red");
+        pc.getMotherboard().loadProgram("windows XP");
+    }
+}
