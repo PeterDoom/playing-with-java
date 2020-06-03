fdb74522f8dbabc04a9a4c45bac61c7ff97d4eb9
PeterDoom <nanev0205@gmail.com>
2020-06-04 00:18:48 +0300

    Extending the example

--- HelloWorld/src/innerClasses/Gearbox.java
+++ HelloWorld/src/innerClasses/Gearbox.java
@@ -15,26 +15,48 @@ public class Gearbox {
         this.gears.add(neutral);
     }
 
-    public void  operateClutch(boolean in) {
+    public void operateClutch(boolean in) {
         this.clutchIsIn = in;
     }
 
     public void addGear(int number, double ratio) {
-        if ((number > 0) && (number<=maxGears)) {
-            this.gears.add(new Gear(number,ratio));
+        if ((number > 0) && (number <= maxGears)) {
+            this.gears.add(new Gear(number, ratio));
         }
+    }
+
+    public void changeGears(int newGear) {
+        if ((newGear >= 0) && (newGear < this.gears.size()) && this.clutchIsIn) {
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected");
+        } else {
+            System.out.println("Grind");
+            this.currentGear = 0;
+        }
+    }
+
+    public double wheelSpeed(int revs) {
+        if (clutchIsIn) {
+            System.out.println("Oh boy, clutch is in");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
     }
 
     public class Gear {
         private int gearNumber;
         private double ratio;
 
+        public double getRatio() {
+            return ratio;
+        }
+
         public Gear(int gearNumber, double ratio) {
             this.gearNumber = gearNumber;
             this.ratio = ratio;
         }
 
-        public double driveSpeed(int revs){
+        public double driveSpeed(int revs) {
             return revs * (this.ratio);
         }
     }
