dd54c39052a82ec6b33bfc8c6c230bd263ecca23
PeterDoom <48176167+PeterDoom@users.noreply.github.com>
2020-06-11 23:23:19 +0300

    Adding a new team and player class to the generics example

---
+++ HelloWorld/src/generics/BasketballPlayer.java
@@ -0,0 +1,11 @@
+package generics;
+
+public class BasketballPlayer extends Player {
+
+    public String name;
+
+    public BasketballPlayer(String name) {
+        super(name);
+    }
+    
+}
--- HelloWorld/src/generics/GenericsMain.java
+++ HelloWorld/src/generics/GenericsMain.java
@@ -4,12 +4,21 @@ public class GenericsMain {
     public static void main(String[] args) {
         FootballPlayer joe = new FootballPlayer("Joe");
         BaseballPlayer pat = new BaseballPlayer("Pat");
+        BasketballPlayer lebron = new BasketballPlayer("LeBron");
+        BasketballPlayer dennis = new BasketballPlayer("Dennis");
 
         Team<FootballPlayer> footballTeam = new Team<>("FootballTeam");
         footballTeam.addPlayer(joe);
 
         Team<BaseballPlayer> baseBallTeam = new Team<>("baseBallTeam");
         baseBallTeam.addPlayer(pat);
+
+        Team<BasketballPlayer> basketballTeam = new Team<>("basketballTeam");
+        Team<BasketballPlayer> basketballTeamTwo = new Team<>("basketballTeamTwo");
+        basketballTeam.addPlayer(lebron);
+        basketballTeamTwo.addPlayer(dennis);
+
+        basketballTeam.matchResult(basketballTeamTwo, 12, 35);
 
         System.out.println(footballTeam.numMembers());
         System.out.println(baseBallTeam.numMembers());
