8fc8291643ff3bdff24fb0b22edddec54bcafcee
PeterDoom <nanev0205@gmail.com>
2020-06-28 00:09:22 +0300

    Adding print func

--- HelloWorld/src/javaCollections/javaCollectionsMain.java
+++ HelloWorld/src/javaCollections/javaCollectionsMain.java
@@ -1,15 +1,20 @@
 package javaCollections;
 
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class javaCollectionsMain {
     public static void main(String[] args) {
-        Theatre theatre = new Theatre("Jix", 50, 20);
+        Theatre theatre = new Theatre("Jix", 12, 20);
         theatre.reserveSeat("A15");
         theatre.reserveSeat("A15");
         theatre.reserveSeat("A235");
+        List<Theatre.Seat> seatCopy = theatre.getSeats();
+        printList(seatCopy);
+        sortList(seatCopy);
+        printList(seatCopy);
     }
 
     public static void sortList(List<? extends Theatre.Seat> list) {
@@ -21,5 +26,13 @@ public class javaCollectionsMain {
             }
 
         }
+    }
+
+    public static void printList(List<Theatre.Seat> list) {
+        for(Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+        System.out.println();
+        System.out.println("======================================================================");
     }
 }
