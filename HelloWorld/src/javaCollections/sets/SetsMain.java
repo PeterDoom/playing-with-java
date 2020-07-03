0f71adaaeac31e2050feed69d8dc8366877c1fb8
PeterDoom <nanev0205@gmail.com>
2020-07-03 23:20:24 +0300

    Instatiating the class

--- HelloWorld/src/javaCollections/sets/SetsMain.java
+++ HelloWorld/src/javaCollections/sets/SetsMain.java
@@ -1,4 +1,18 @@
 package javaCollections.sets;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class SetsMain {
+
+    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Set<HeavenlyBody> planets = new HashSet<>();
+
+    public static void main(String[] args) {
+        HeavenlyBody temp = new HeavenlyBody("Mercury", 88);
+        solarSystem.put(temp.getName(), temp);
+    }
+
 }
