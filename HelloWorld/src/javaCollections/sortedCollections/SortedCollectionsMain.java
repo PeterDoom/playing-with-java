1d522abc5d8e5714a98108137974f0dc4a5c13fa
PeterDoom <nanev0205@gmail.com>
2020-08-01 19:44:27 +0300

    Adding a Basket class in order to start working in the Main Class

--- HelloWorld/src/javaCollections/sortedCollections/SortedCollectionsMain.java
+++ HelloWorld/src/javaCollections/sortedCollections/SortedCollectionsMain.java
@@ -1,4 +1,10 @@
 package javaCollections.sortedCollections;
 
 public class SortedCollectionsMain {
+    private static StockList stockList = new StockList();
+
+    public static void main(String[] args) {
+        StockItem temp = new StockItem("bread", 1.10, 100);
+        stockList.addStock(temp);
+    }
 }
