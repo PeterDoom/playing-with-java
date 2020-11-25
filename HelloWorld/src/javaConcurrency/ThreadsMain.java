b6bd3eefd3cbf9cca8060e53e5d79ee540722a30
PeterDoom <nanev0205@gmail.com>
2020-11-25 23:15:21 +0200

    Adding a space for visual separation

--- HelloWorld/src/javaConcurrency/ThreadsMain.java
+++ HelloWorld/src/javaConcurrency/ThreadsMain.java
@@ -3,6 +3,7 @@ package javaConcurrency;
 public class ThreadsMain {
     public static void main(String[] args) {
         System.out.println("Hello from the main thread");
+
         Thread anotherThread = new AnotherThread();
         anotherThread.start();
     }
