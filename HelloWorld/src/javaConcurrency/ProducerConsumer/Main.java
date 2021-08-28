4ab090dc46684173ddde612703ae5d77533131d2
PeterDoom <nanev0205@gmail.com>
2021-08-28 21:17:14 +0300

    Synchronising the buffer addition

--- HelloWorld/src/javaConcurrency/ProducerConsumer/Main.java
+++ HelloWorld/src/javaConcurrency/ProducerConsumer/Main.java
@@ -1,7 +1,17 @@
 package javaConcurrency.ProducerConsumer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> buffer = new ArrayList<String>();
+        MyProducer producer1 = new MyProducer(buffer, ThreadColor.ANSI_BLUE);
+        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_GREEN);
+        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN);
 
+        new Thread(producer1).start();
+        new Thread(consumer1).start();
+        new Thread(consumer2).start();
     }
 }
--- HelloWorld/src/javaConcurrency/ProducerConsumer/MyProducer.java
+++ HelloWorld/src/javaConcurrency/ProducerConsumer/MyProducer.java
@@ -18,7 +18,9 @@ public class MyProducer implements Runnable {
         for (String num : nums) {
             try {
                 System.out.println(color + "Adding..." + num);
-                buffer.add(num);
+                synchronized (buffer) {
+                    buffer.add(num);
+                }
                 Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e) {
                 System.out.println("Producer was interrupted");
@@ -26,6 +28,8 @@ public class MyProducer implements Runnable {
             }
         }
         System.out.println(color + "Adding EOF and exiting...");
-        buffer.add("EOF");
+        synchronized (buffer) {
+            buffer.add("EOF");
+        }
     }
 }
