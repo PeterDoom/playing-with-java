a10297f4ee8587cd9c322b788774310661064f3b
Plamen Nanev <48176167+PeterDoom@users.noreply.github.com>
2021-08-30 23:33:31 +0300

    Update Main.java

--- HelloWorld/src/javaConcurrency/ProducerConsumer/Main.java
+++ HelloWorld/src/javaConcurrency/ProducerConsumer/Main.java
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args) {
         List<String> buffer = new ArrayList<String>();
         ReentrantLock bufferLock = new ReentrantLock();
-        MyProducer producer1 = new MyProducer(buffer, ThreadColor.ANSI_BLUE, bufferLock);
+        MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_BLUE, bufferLock);
         MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_GREEN, bufferLock);
         MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_RED, bufferLock);
 
-        new Thread(producer1).start();
+        new Thread(producer).start();
         new Thread(consumer1).start();
         new Thread(consumer2).start();
     }
