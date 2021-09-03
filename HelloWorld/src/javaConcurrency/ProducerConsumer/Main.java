88e04d6182e6b6bef2fa3525c4f158c3542c3717
PeterDoom <nanev0205@gmail.com>
2021-09-03 23:57:09 +0300

    Adding a future

--- HelloWorld/src/javaConcurrency/ProducerConsumer/Main.java
+++ HelloWorld/src/javaConcurrency/ProducerConsumer/Main.java
@@ -1,10 +1,9 @@
 package javaConcurrency.ProducerConsumer;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
@@ -21,6 +20,22 @@ public class Main {
         executorService.execute(producer);
         executorService.execute(consumer1);
         executorService.execute(consumer2);
+
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColor.ANSI_PURPLE + "I'm being printed");
+                return "This is the callableResult";
+            }
+        });
+
+        try {
+            System.out.println(future.get());
+        } catch (ExecutionException e) {
+            System.out.println("Something went wrong");
+        } catch (InterruptedException e) {
+            System.out.println("Thread running the task was interrupted");
+        }
 
         executorService.shutdown();
     }
