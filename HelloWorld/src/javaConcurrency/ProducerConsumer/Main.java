4a6215435669406732e491f8823a95b18c667d64
PeterDoom <nanev0205@gmail.com>
2021-09-03 23:44:40 +0300

    Adding a threadpool

--- HelloWorld/src/javaConcurrency/ProducerConsumer/Main.java
+++ HelloWorld/src/javaConcurrency/ProducerConsumer/Main.java
@@ -2,12 +2,18 @@ package javaConcurrency.ProducerConsumer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     public static void main(String[] args) {
         List<String> buffer = new ArrayList<String>();
         ReentrantLock bufferLock = new ReentrantLock();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
         MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_BLUE, bufferLock);
         MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_GREEN, bufferLock);
         MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_RED, bufferLock);
