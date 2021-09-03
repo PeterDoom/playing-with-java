9092b452d2acb0300007b9c58f366176ec487e33
PeterDoom <nanev0205@gmail.com>
2021-09-03 23:59:46 +0300

    Adding more threads

--- HelloWorld/src/javaConcurrency/ProducerConsumer/Main.java
+++ HelloWorld/src/javaConcurrency/ProducerConsumer/Main.java
@@ -11,7 +11,7 @@ public class Main {
         List<String> buffer = new ArrayList<String>();
         ReentrantLock bufferLock = new ReentrantLock();
 
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
 
         MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_BLUE, bufferLock);
         MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_GREEN, bufferLock);
