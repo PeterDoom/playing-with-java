cd30052b5ad0a5890bdef63fd822db594c39e434
PeterDoom <nanev0205@gmail.com>
2021-09-14 23:55:35 +0300

    Adding a static class

--- HelloWorld/src/javaConcurrency/ThreadStarvationExample/ThreadStarvationExample.java
+++ HelloWorld/src/javaConcurrency/ThreadStarvationExample/ThreadStarvationExample.java
@@ -4,6 +4,19 @@ public class ThreadStarvationExample {
     private static Object lock = new Object();
 
     public static void main(String[] args) {
+    }
 
+    private static class Worker implements Runnable {
+        private int runCount = 1;
+        private String threadColor;
+
+        public Worker(String threadColor) {
+            this.threadColor = threadColor;
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 }
