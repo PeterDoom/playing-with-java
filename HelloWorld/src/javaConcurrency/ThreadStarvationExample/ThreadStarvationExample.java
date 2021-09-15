c4f9c42649490a61dc6e201765959c5d840d7e9c
PeterDoom <nanev0205@gmail.com>
2021-09-15 23:20:23 +0300

    Adding 4 threads to showcase the example

--- HelloWorld/src/javaConcurrency/ThreadStarvationExample/ThreadStarvationExample.java
+++ HelloWorld/src/javaConcurrency/ThreadStarvationExample/ThreadStarvationExample.java
@@ -1,9 +1,25 @@
 package javaConcurrency.ThreadStarvationExample;
 
+import javaConcurrency.ProducerConsumer.ThreadColor;
+
 public class ThreadStarvationExample {
     private static Object lock = new Object();
 
     public static void main(String[] args) {
+        Thread t1 = new Thread(new Worker(ThreadColor.ANSI_RED), "Priority 10");
+        Thread t2 = new Thread(new Worker(ThreadColor.ANSI_BLUE), "Priority 8");
+        Thread t3 = new Thread(new Worker(ThreadColor.ANSI_GREEN), "Priority 6");
+        Thread t4 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "Priority 4");
+
+        t1.setPriority(10);
+        t2.setPriority(8);
+        t3.setPriority(6);
+        t4.setPriority(4);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
     }
 
     private static class Worker implements Runnable {
@@ -16,7 +32,11 @@ public class ThreadStarvationExample {
 
         @Override
         public void run() {
-
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    System.out.format(threadColor + "%s: runCount = %d\n", Thread.currentThread().getName(), runCount++);
+                }
+            }
         }
     }
 }
